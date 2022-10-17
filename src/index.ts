bf08d6082c5dfc0f78e68d165c65760d37169606
Anillc <i@anillc.cn>
2022-10-18 03:07:31 +0800

    update deps

--- src/index.ts
+++ src/index.ts
@@ -9,17 +9,19 @@ declare module 'koishi' {
 const logger = new Logger('forward')
 
 const forwarding: Record<string, Array<string>> = {}
-const messageRecord: Array<[string, string, [string, string], [string, string]]> = []
+// cid, messageId
+const messageRecord: Array<[[string, string], [string, string]]> = []
 
 export const name = 'forward'
 
+export const using = ['database']
+
 export async function apply(ctx:Context) {
     ctx.model.extend('channel', { forward: 'list' })
-    await new Promise(cb => ctx.using(['database'], cb))
     const channels = await ctx.database.get('channel', {}, ['platform', 'id', 'forward'])
     channels.forEach(channel => forwarding[`${channel.platform}:${channel.id}`] = channel.forward)
     ctx.middleware(mid(ctx))
-    ctx.command('forward <to>','消息转发', { authority: 2 })
+    ctx.command('forward [to]','消息转发', { authority: 2 })
         .option('remove', '-r 删除转发')
         .option('list', '-l 查看转发列表')
         .option('info', '-i 查看本群 id')
@@ -27,94 +29,80 @@ export async function apply(ctx:Context) {
         .action(({ session, options }, to) => {
             if (!session.channelId) return '请在群聊中使用该指令'
             const forward = session.channel.forward
-            const id = `${session.platform}:${session.channelId}`
 
+            console.log(123);
+            
             if (options.list) return JSON.stringify(forward)
-            if (options.info) return id
+            if (options.info) return session.cid
             if (!to) return '缺少必要参数'
 
             const index = forward.indexOf(to)
             if (options.remove) {
                 if (index == -1) return '没有转发到该频道的记录'
                 forward.splice(index, 1)
-                forwarding[id] = forward
+                forwarding[session.cid] = forward
                 return '删除成功!'
             }
             if (index >= 0) return '已经存在记录'
             forward.push(to);
-            forwarding[id] = forward
+            forwarding[session.cid] = forward
             return '添加成功!'
         });
 }
 
-function ignore(chain: segment.Chain){
-    if (chain[0].type === 'quote') {
-        chain = chain.slice(1)
-    }
-    return segment.join(chain).trim().startsWith('//')
-}
-
 function mid(ctx: Context) {
     return function (session: Session, next: () => void) {
-        const chain = segment.parse(session.content)
-        if (!session.channel || ignore(chain)) return next()
-        const forward = forwarding[`${session.platform}:${session.channelId}`]
-        forward?.forEach(async to => {
+        const forward = forwarding[session.cid]
+        if (!session.channel || session.content.startsWith('//') || !forward) return next()
+        forward.forEach(async to => {
             const [toPlatform, toChannelId] = to.split(':')
-            const quote = chain?.[0]
-            let start = 0
-
-            // transform quote
-            if(quote?.type === 'quote') {
-                start++
-                const rec = messageRecord.find(([platform1, platform2, [id1], [id2]]) =>
-                    platform1 === session.platform && platform2 === toPlatform && id1 === quote.data.id ||
-                    platform1 === toPlatform && platform2 === session.platform && id2 === quote.data.id)
-                const data = rec?.[2][0] === quote.data.id ? rec?.[3] : rec?.[2]
-                if (!data) return
-                chain[0] = {
-                    type: 'quote',
-                    data: {
-                        id: data[0],
-                        channelId: data[1],
-                    },
-                }
-            }
+            const elements = segment.parse(session.content)
 
             // transform images
-            for (const i in chain) {
-                const seg = chain[i]
-                if (seg.type !== 'image' || !seg.data.url?.startsWith('http')) continue
+            for (const element of elements) {
+                if (element.type === 'face' && element.attrs.url) element.type = 'image'
+                if (element.type !== 'image' || !element.attrs.url?.startsWith('http')) continue
                 try {
-                    const data = await ctx.http.get(seg.data.url, {
-                        responseType: 'arraybuffer'
-                    })
+                    const data = await ctx.http.get(element.attrs.url, { responseType: 'arraybuffer' })
                     const img = Buffer.from(data).toString('base64')
-                    chain[i] = {
-                        type: 'image',
-                        data: {
-                            url: `base64://${img}`,
-                        }
-                    }
+                    element.attrs.url = `base64://${img}`
                 } catch (e) {
                     logger.info('error while transforming images', e)
                 }
             }
 
-            chain.splice(start, 0, {
-                type: 'text',
-                data: {
-                    content: `${session.username}: `,
-                },
-            })
-            ctx.broadcast([to], segment.join(chain)).then(([id]) => {
-                messageRecord.push([session.platform, toPlatform, [session.messageId, session.channelId], [id, toChannelId]])
-                if (messageRecord.length > 1000) {
-                    messageRecord.shift()
+            elements.unshift(segment('text', {
+                content: `${session.username}: `
+            }))
+
+            if(session.quote) {
+                let target: [string, string]
+                for (const [a, b] of messageRecord) {
+                    if (tupleEqual([session.cid, session.quote.messageId], a)) {
+                        target = b
+                        break
+                    }
+                    if (tupleEqual([session.cid, session.quote.messageId], b)) {
+                        target = a
+                        break
+                    }
                 }
+                if (target) {
+                    elements.unshift(segment.quote(target[1], { channelId: toChannelId }))
+                }
+            }
+
+            ctx.broadcast([to], elements.join('')).then((ids) => {
+                if (ids.length === 0) return
+                messageRecord.push([[session.cid, session.messageId], [to, ids[ids.length - 1]]])
+                if (messageRecord.length > 10000) messageRecord.shift()
             })
         })
         logger.debug(`forwarding: ${JSON.stringify(forward)} ${session.content}`)
         return next()
     }
+}
+
+function tupleEqual([a, b]: [string, string], [c, d]: [string, string]) {
+    return a === c && b === d
 }
